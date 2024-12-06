9df8b36842ba3c7153f72568d8b1d0c71afe0a8c
Thang X. Vu <thang@coongcrafts.io>
2024-12-06 12:03:05 +0700

    log const

--- e2e/zombienet/src/setup.ts
+++ e2e/zombienet/src/setup.ts
@@ -3,9 +3,24 @@ import { DedotClient, PinnedBlock, WsProvider } from 'dedot';
 
 const CONTRACTS_NODE_ENDPOINT = 'ws://127.0.0.1:9944';
 
+const getConst = (pallet: string, name: string) => {
+  try {
+    return client.consts[pallet][name];
+  } catch {
+    // ignore this on purpose
+  }
+
+  return undefined;
+};
+
 beforeAll(async () => {
   console.log(`Connect to ${CONTRACTS_NODE_ENDPOINT}`);
   global.client = await DedotClient.new(new WsProvider(CONTRACTS_NODE_ENDPOINT));
+
+  console.log('system.blockHashCount', getConst('system', 'blockHashCount'));
+  console.log('babe.expectedBlockTime', getConst('babe', 'expectedBlockTime'));
+  console.log('aura.slotDuration', getConst('aura', 'slotDuration'));
+  console.log('timestamp.minimumPeriod', getConst('timestamp', 'minimumPeriod'));
 
   return new Promise((resolve) => {
     global.client.chainHead.on('finalizedBlock', (x: PinnedBlock) => {
