b0e47a8a168cf212e3a91e054c9b76418007c299
1cedrus <aowiej23s@gmail.com>
2024-12-06 21:00:56 +0700

    reset useContract

--- e2e/zombienet/src/hooks/useContract.test.ts
+++ e2e/zombienet/src/hooks/useContract.test.ts
@@ -1,12 +1,9 @@
 import { beforeAll, describe, expect, it } from 'vitest';
-import { ALICE, deployFlipperContract } from '../utils';
+import { ALICE, deployFlipperContract } from '../utils.js';
 import { numberToHex } from 'dedot/utils';
 import { Contract } from 'dedot/contracts';
-import { FlipperContractApi } from 'contracts/flipper';
-import { BOB, deployPSP22Contract, devPairs, mintifyPSP22Balance, wrapper } from '../utils.js';
-import { usePSP22Balance } from 'typink';
+import { FlipperContractApi } from '../contracts/flipper';
 import * as flipper from '../contracts/flipper_v5.json';
-import { renderHook, waitFor } from '@testing-library/react';
 
 describe('useContract', () => {
   let contract: Contract<FlipperContractApi>;
@@ -22,97 +19,5 @@ describe('useContract', () => {
 
     console.log(`Initial value:`, state);
     expect(state).toBe(true);
-  });
-});
-
-describe('usePSP22Balance', () => {
-  let contractAddress: string;
-  beforeAll(async () => {
-    const { alice, bob } = devPairs();
-
-    const salt = numberToHex(Date.now());
-    contractAddress = await deployPSP22Contract(salt);
-
-    await mintifyPSP22Balance(contractAddress, alice, 100);
-    await mintifyPSP22Balance(contractAddress, bob, 200);
-  });
-
-  it('should load balance properly', async () => {
-    const { result } = renderHook(({ address }) => usePSP22Balance({ contractAddress, address }), {
-      wrapper,
-      initialProps: {
-        address: ALICE,
-      },
-    });
-
-    expect(result.current.data).toBeUndefined();
-
-    await waitFor(() => {
-      expect(result.current.data).toBeDefined();
-    });
-
-    expect(result.current?.data).toBeGreaterThan(0n);
-  });
-
-  it('should update balance when address changes', async () => {
-    const { result, rerender } = renderHook(({ address }) => usePSP22Balance({ contractAddress, address }), {
-      wrapper,
-      initialProps: {
-        address: ALICE,
-      },
-    });
-
-    // Wait for ALICE's balance to be fetched
-    await waitFor(() => {
-      expect(result.current.data).toBeDefined();
-    });
-
-    const aliceBalance = result.current;
-
-    rerender({ address: BOB });
-
-    // Initially, the balance should be undefined again
-    expect(result.current.data).toBeUndefined();
-
-    // Wait for BOB's balance to be fetched
-    await waitFor(() => {
-      expect(result.current.data).toBeDefined();
-    });
-
-    // BOB's balance should be different from ALICE's
-    expect(result.current.data).not.toEqual(aliceBalance);
-  });
-
-  it('should return undefined for invalid address', async () => {
-    const { result } = renderHook(() => usePSP22Balance({ contractAddress, address: 'invalid_address' }), { wrapper });
-
-    // The balance should remain undefined
-    await waitFor(
-      () => {
-        expect(result.current.data).toBeUndefined();
-      },
-      { timeout: 5000 },
-    );
-  });
-
-  it('should automatic update balance when watch is enabled', async () => {
-    const { result } = renderHook(() => usePSP22Balance({ contractAddress, address: ALICE, watch: true }), { wrapper });
-
-    // Wait for ALICE's balance to be fetched
-    await waitFor(() => {
-      expect(result.current.data).toBeDefined();
-    });
-
-    const aliceBalance = result.current.data;
-
-    // Simulate a transfer event
-    const { alice } = devPairs();
-    await mintifyPSP22Balance(contractAddress, alice, 100);
-
-    // Wait for the balance to be updated
-    await waitFor(() => {
-      expect(result.current.data).toBeDefined();
-      expect(result.current.data).not.toEqual(aliceBalance);
-    });
   });
 });
