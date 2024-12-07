5d6de7b5f38ddc12d1baf0aa808cc39475972e73
Thang X. Vu <zthangxv@gmail.com>
2024-12-07 13:33:38 +0700

    remove happy-dom

--- e2e/zombienet/vitest.config.ts
+++ e2e/zombienet/vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // @ts-ignore
   test: {
-    environment: 'happy-dom',
+    // environment: 'happy-dom',
     setupFiles: './src/setup.ts',
     testTimeout: 120_000,
     hookTimeout: 120_000,
