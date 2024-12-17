9948ac9d8fc8ed28fc27f28dbaf3b67856733863
Thang X. Vu <thang@coongcrafts.io>
2024-12-17 10:00:20 +0700

    kick off

--- e2e/zombienet/src/setup.ts
+++ e2e/zombienet/src/setup.ts
@@ -1,13 +1,36 @@
 import { afterAll, beforeAll } from 'vitest';
 import { DedotClient, PinnedBlock, WsProvider } from 'dedot';
+import { BOB, devPairs, transferNativeBalance } from 'utils';
 
 const CONTRACTS_NODE_ENDPOINT = 'ws://127.0.0.1:9944';
+
+export const sleep = (ms: number = 0) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const kickOff = async () => {
+  const { alice } = devPairs();
+
+  while (true) {
+    const nonce = (await client.query.system.account(alice.address)).nonce;
+    if (nonce > 0) {
+      break;
+    }
+
+    try {
+      await transferNativeBalance(alice, BOB, BigInt(1e12));
+    } catch (e) {
+      console.log(e);
+      await sleep(1000);
+    }
+  }
+};
 
 beforeAll(async () => {
   console.log(`Connect to ${CONTRACTS_NODE_ENDPOINT}`);
   global.client = await DedotClient.new(new WsProvider(CONTRACTS_NODE_ENDPOINT));
 
-  return new Promise((resolve) => {
+  await new Promise((resolve) => {
     global.client.chainHead.on('finalizedBlock', (x: PinnedBlock) => {
       console.log('Current finalized block number:', x.number);
 
@@ -16,7 +39,9 @@ beforeAll(async () => {
       }
     });
   });
-}, 120_000);
+
+  await kickOff();
+}, 300_000);
 
 afterAll(async () => {
   await global.client.disconnect();
