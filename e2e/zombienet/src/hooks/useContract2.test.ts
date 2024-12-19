e2f06a9de6cd372cf8b0b84f20e2e3a89a657e1b
Thang X. Vu <thang@coongcrafts.io>
2024-12-19 09:36:41 +0700

    add more psp22 tests

--- e2e/zombienet/src/hooks/useContract2.test.ts
+++ e2e/zombienet/src/hooks/useContract2.test.ts
@@ -1,5 +1,5 @@
 import { beforeAll, describe, expect, it } from 'vitest';
-import { ALICE, deployPsp22Contract, psp22Metadata, wrapper } from '../utils';
+import { ALICE, BOB, deployPsp22Contract, psp22Metadata, wrapper } from '../utils';
 import { numberToHex } from 'dedot/utils';
 import { Contract } from 'dedot/contracts';
 import { Psp22ContractApi } from '../contracts/psp22';
@@ -7,31 +7,31 @@ import { renderHook, waitFor } from '@testing-library/react';
 import { usePSP22Balance } from 'typink';
 
 describe('useContract2', () => {
-  let contract: Contract<Psp22ContractApi>;
+  let contractAddress: string, contract: Contract<Psp22ContractApi>;
   beforeAll(async () => {
     const randomSalt = numberToHex(Date.now());
-    const address = await deployPsp22Contract(randomSalt);
-    console.log('Deployed contract address', address);
-    contract = new Contract<Psp22ContractApi>(client, psp22Metadata, address, { defaultCaller: ALICE });
+    contractAddress = await deployPsp22Contract(randomSalt);
+    console.log('Deployed contract address', contractAddress);
+    contract = new Contract<Psp22ContractApi>(client, psp22Metadata, contractAddress, { defaultCaller: ALICE });
   });
 
   it('get total supply', async () => {
     const { data: state } = await contract.query.psp22TotalSupply();
 
     console.log(`Total supply`, state);
-    expect(state).toBeDefined();
+    expect(state).toBe(BigInt(1e20));
   });
 
   it('get alice balance', async () => {
     const { data: state } = await contract.query.psp22BalanceOf(ALICE);
 
     console.log(`alice balance`, state);
-    expect(state).toBeDefined();
+    expect(state).toBe(BigInt(1e20));
   });
 
   it('should load balance properly', async () => {
     const { result } = renderHook(
-      () => usePSP22Balance({ contractAddress: contract.address.address(), address: ALICE }), // prettier-end-here
+      () => usePSP22Balance({ contractAddress, address: ALICE }), // prettier-end-here
       { wrapper },
     );
 
@@ -41,6 +41,35 @@ describe('useContract2', () => {
       expect(result.current.data).toBeDefined();
     });
 
-    expect(result.current.data).toBeGreaterThan(0n);
+    expect(result.current.data).toBeGreaterThan(BigInt(1e20));
+  });
+
+  it('should update balance when address changes', async () => {
+    const { result, rerender } = renderHook(({ address }) => usePSP22Balance({ contractAddress, address }), {
+      wrapper,
+      initialProps: {
+        address: ALICE,
+      },
+    });
+
+    expect(result.current.isLoading).toBe(true);
+
+    // Wait for ALICE's balance to be fetched
+    await waitFor(() => {
+      expect(result.current.data).toBeDefined();
+      expect(result.current.isLoading).toBe(false);
+    });
+
+    const aliceBalance = result.current.data;
+
+    rerender({ address: BOB });
+
+    // Wait for BOB's balance to be fetched
+    await waitFor(() => {
+      expect(result.current.data).toBe(0n);
+    });
+
+    // BOB's balance should be different from ALICE's
+    expect(result.current.data).not.toEqual(aliceBalance);
   });
 });
