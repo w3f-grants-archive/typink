be150097b1a9d31582cf399e51c345d977468769
Thang X. Vu <thang@coongcrafts.io>
2024-12-22 14:49:31 +0700

    update tests for useContractQuery

--- e2e/zombienet/src/hooks/useContractQuery.test.ts
+++ e2e/zombienet/src/hooks/useContractQuery.test.ts
@@ -1,8 +1,8 @@
 import { beforeAll, describe, expect, it } from 'vitest';
-import { ALICE, BOB, deployPsp22Contract, devPairs, mintPSP22Balance, psp22Metadata, wrapper } from '../utils';
+import { ALICE, BOB, CHARLIE, deployPsp22Contract, psp22Metadata, wrapper } from '../utils';
 import { numberToHex } from 'dedot/utils';
 import { renderHook, waitFor } from '@testing-library/react';
-import { useContractQuery, usePSP22Balance } from 'typink';
+import { useContractQuery } from 'typink';
 import { Contract } from 'dedot/contracts';
 import { Psp22ContractApi } from 'contracts/psp22';
 
@@ -39,6 +39,29 @@ describe('useContractQuery', () => {
 
     await waitFor(() => {
       expect(result.current.data?.isOk).toBe(true);
+    });
+  });
+
+  it('should fail dry-run', async () => {
+    const { result } = renderHook(
+      () =>
+        useContractQuery({
+          contract,
+          fn: 'psp22Transfer',
+          args: [BOB, BigInt(1e12), '0x'],
+          options: { caller: CHARLIE },
+        }),
+      {
+        wrapper,
+      },
+    );
+
+    expect(result.current.data).toBeUndefined();
+
+    await waitFor(() => {
+      expect(result.current.data?.isErr).toBe(true);
+      // @ts-ignore
+      expect(result.current.data?.err).toEqual({ type: 'InsufficientBalance' });
     });
   });
 });
