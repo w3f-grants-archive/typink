3c3066fc60b9e1bdb3b037f986be417ec7fc7260
Thang X. Vu <thang@dedot.dev>
2024-12-31 10:48:14 +0700

    feat: add more contract networks (#93)
    * add contract networks
    
    * fix specs
    
    * update network assets

--- packages/typink/src/networks/development.ts
+++ packages/typink/src/networks/development.ts
@@ -5,7 +5,6 @@ export const development: NetworkInfo = {
   name: 'Development',
   logo: `https://avatars.githubusercontent.com/u/47530779?s=200&v=4`,
   provider: 'ws://127.0.0.1:9944',
-  prefix: 42,
   symbol: 'UNIT',
   decimals: 12,
 };
--- packages/typink/src/networks/mainnet.ts
+++ packages/typink/src/networks/mainnet.ts
@@ -1,1 +1,29 @@
-export {};
+import { JsonRpcApi, NetworkInfo } from '../types';
+
+export const alephZero: NetworkInfo = {
+  id: 'alephzero',
+  name: 'Aleph Zero',
+  logo: 'https://raw.githubusercontent.com/dedotdev/typink/refs/heads/main/assets/networks/alephzero.svg',
+  provider: 'wss://ws.azero.dev',
+  symbol: 'AZERO',
+  decimals: 12,
+  jsonRpcApi: JsonRpcApi.LEGACY,
+};
+
+export const astar: NetworkInfo = {
+  id: 'astar',
+  name: 'Astar',
+  logo: 'https://raw.githubusercontent.com/dedotdev/typink/refs/heads/main/assets/networks/astar.png',
+  provider: 'wss://rpc.astar.network',
+  symbol: 'ASTR',
+  decimals: 18,
+};
+
+export const shiden: NetworkInfo = {
+  id: 'astar_shiden',
+  name: 'Shiden',
+  logo: 'https://raw.githubusercontent.com/dedotdev/typink/refs/heads/main/assets/networks/shiden.png',
+  provider: 'wss://rpc.shiden.astar.network',
+  symbol: 'SDN',
+  decimals: 18,
+};
